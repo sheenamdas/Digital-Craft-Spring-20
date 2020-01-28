391d2bf0d66095a0fba57ee7d66b143adae06470
sheenamdas <60147010+sheenamdas@users.noreply.github.com>
2020-01-28 11:35:59 -0500

    Add files via upload

---
+++ class work, 28:1/sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+//will conitnually run
+function draw() {
+
+  //ellipse(x position, y position, width, height);
+ 
+  strokeWeight(6);
+  fill(200);
+  rect(0,0, 70, 30);
+  rect(70,0, 100, 30);
+  rect(170, 0, 70, 30);
+  fill(130);
+  rect(0, 30, 30, 60);
+  rect(0, 90, 30, 100);
+  rect(0, 190, 30, 60);
+  fill(100, 0, 0);
+  rect(30,30, 140, 140);
+  fill(300,300,10);
+  rect(170, 30, 70, 60);
+  rect(170, 90, 35, 80); 
+  rect(205, 90, 35, 80); 
+  
+  rect(170, 170, 70, 50);
+  rect(30, 170, 50, 50);
+  rect(80, 170, 90,25);
+  rect(80, 195, 90,25);
+}
