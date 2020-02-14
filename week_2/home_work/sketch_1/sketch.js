fabeabaf00ec60184aa4903ef9d263338b3d5396
sheenamdas <60147010+sheenamdas@users.noreply.github.com>
2020-02-14 12:53:44 -0500

    .

--- week_2/home_work/sketch_1/sketch.js
+++ week_2/home_work/sketch_1/sketch.js
@@ -9,7 +9,23 @@ function setup() {
 
 function draw() {
 
- fill(700);
+//  fill(700);
+//   rect(0,0, 820, 720);
+//   fill(1)
+//     strokeWeight(0);
+//   fill(100,100,100);
+//    //main rect that is static
+//   rect(100,100,500,500)
+   
+// //little squares that move
+//   fill(12);
+//  rect(mouseX,400,100,100)
+//   rect(mouseX,400,100,100)
+
+//   rect(300,mouseY,100,100);
+//   rect(300,mouseY*2,100,100);
+//    rect(300,mouseY/2,100,100);
+  fill(700);
   rect(0,0, 820, 720);
   fill(1)
     strokeWeight(0);
@@ -19,14 +35,19 @@ function draw() {
    
 //little squares that move
   fill(12);
- rect(mouseX,400,100,100)
-  rect(mouseX,400,100,100)
+  for (x = 100; x < width; x+=200){
+    for (y = 100; y < height; y+= 200){
+      
+ rect(mouseX*2,y,100,100)
+  rect(mouseX,y,100,100)
+      rect(mouseX/2, y, 100, 100);
 
-  rect(300,mouseY,100,100);
-  rect(300,mouseY*2,100,100);
-   rect(300,mouseY/2,100,100);
+  rect(x,mouseY,100,100);
+  rect(x,mouseY*2,100,100);
+   rect(x,mouseY/2,100,100);
+    }
   
-
+  }
   
 
 
